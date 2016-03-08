68fa0f60a3ab109945fa435d3541acda9ad01e8a
Dmitry Shpika <scrptn@gmail.com>
2016-03-09 00:12:52 +0300

    Add custom style for translation list

--- src/components/TranslationList.jsx
+++ src/components/TranslationList.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Translation from './Translation'
 
+require('../styles/translation-list.less')
+
 const TranslationList = ({ translations }) => (
-  <ol>
+  <ol className='translation-list'>
     { translations.map((t, idx) => <Translation key={idx} translation={t} />) }
   </ol>
 )
