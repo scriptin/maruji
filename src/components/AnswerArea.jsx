6b3c7a17b3c1c0fb5ef4aa059c2d5f5ee4ea6e35
Dmitry Shpika <scrptn@gmail.com>
2016-03-13 00:57:47 +0300

    Add styling and kanji frame to SVG buttons

--- src/components/AnswerArea.jsx
+++ src/components/AnswerArea.jsx
@@ -5,9 +5,26 @@ import SvgButton from './SvgButton'
 import ProgressBar from './ProgressBar'
 import * as util from '../util'
 
+require('../styles/kanji-svg.less')
+
+const buildFrame = (width, height) => {
+  let w = width - 1
+  let h = height - 1
+  let rect = '<rect x="0.5" y="0.5" width="' + w +'" height="' + h + '"/>'
+  let diag1 = '<path d="M0.5,0.5L' + w + ',' + h + '"/>'
+  let diag2 = '<path d="M0.5,' + h + 'L' + w + ',0.5"/>'
+  return $('<g class="frame">' + rect + diag1 + diag2 + '</g>')
+}
+
 const preprocessSvg = (svg, size) => {
-  svg.attr({ width: size, height: size })
-  svg.find('> g:nth-child(2)').remove() // remove stroke numbers
+  // remove stroke numbers:
+  svg.find('> g:nth-child(2)').remove()
+  // remove inline styles from strokes, add a class instead:
+  svg.find('> g:nth-child(2)').removeAttr('style').addClass('strokes')
+  // add frame:
+  svg.prepend(buildFrame(svg.attr('width'), svg.attr('height')))
+  // set class and adjust attributes of a root element:
+  svg.addClass('kanji-svg').attr({ width: size, height: size })
   return svg
 }
 
