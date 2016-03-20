a0465a565d1ee35451cd1c26a6d3b19ce62c0f0e
Dmitry Shpika <scrptn@gmail.com>
2016-03-20 19:14:25 +0300

    KanjiButton: rename internal constants; Rename btn-kanji.less to kanji-button.less

--- src/components/KanjiButton.jsx
+++ src/components/KanjiButton.jsx
@@ -2,19 +2,19 @@ import React, { PropTypes } from 'react'
 import $ from 'jquery'
 import SvgPanel from './SvgPanel'
 
-require('../styles/btn-kanji.less')
+require('../styles/kanji-button.less')
 
-const SvgButton = ({ svg, answerId, answered, correct, active, onAnswerButtonClick }) => {
+const KanjiButton = ({ svg, answerId, answered, correct, active, onAnswerButtonClick }) => {
   let stateClass = 'btn-' + (answered ? (correct ? 'success' : 'danger') : 'default')
   let clickHandler = () => onAnswerButtonClick(answerId)
   return (
-    <button className={'btn btn-kanji ' + stateClass} disabled={ ! active} onClick={clickHandler}>
+    <button className={'kanji-button btn ' + stateClass} disabled={ ! active} onClick={clickHandler}>
       <SvgPanel svg={svg} />
     </button>
   )
 }
 
-SvgButton.propTypes = {
+KanjiButton.propTypes = {
   svg: PropTypes.object.isRequired,
   answerId: PropTypes.number.isRequired,
   answered: PropTypes.bool.isRequired,
@@ -23,4 +23,4 @@ SvgButton.propTypes = {
   onAnswerButtonClick: PropTypes.func.isRequired
 }
 
-export default SvgButton
+export default KanjiButton
