451c5895f99d4a2c88a6f022ed11f7782b575291
Dmitry Shpika <scrptn@gmail.com>
2016-03-27 00:13:51 +0300

    Take number of strokes into account when finding similar kanji, change filter limit criteria

--- build-kanji-meta.js
+++ build-kanji-meta.js
@@ -26,9 +26,11 @@ const parseXML = xmlString => new Promise((resolve, reject) => {
   )
 })
 
+const getSvgRoot = svg => svg.svg.g[0].g[0]
 const containsGroups = g => g.g && _.isArray(g.g) && ! _.isEmpty(g.g)
+const containsPaths = g => g.path && _.isArray(g.path) && ! _.isEmpty(g.path)
 
-const transformSvgGroup = (g, level) => {
+const extractSvgGroups = (g, level) => {
   let elem = g.$['kvg:element'] || g.$['kvg:phon'] || '？'
   let id = g.$.id.replace('kvg:', '')
   let groupNumber = _.padStart(id.split('-g')[1], 3, '0')
@@ -36,13 +38,27 @@ const transformSvgGroup = (g, level) => {
   let key = [level, groupNumber, elem, part, id].join(':')
   let value = null
   if (containsGroups(g)) {
-    value = g.g.map(group => transformSvgGroup(group, level + 1)).filter(_.identity)
+    value = g.g.map(group => extractSvgGroups(group, level + 1)).filter(_.identity)
     if (_.isEmpty(value)) value = null
   }
   return value ? [key].concat(_.flatten(value)) : [key]
 }
 
-const decompose = svg => _.tail(transformSvgGroup(svg.svg.g[0].g[0], 0)).sort()
+const decompose = svg => _.tail(extractSvgGroups(getSvgRoot(svg), 0)).sort()
+
+const extractSvgPathCount = svg => {
+  let selfStrokes = 0
+  let childrenStrokes = 0
+  if (containsPaths(svg)) {
+    selfStrokes = svg.path.length
+  }
+  if (containsGroups(svg)) {
+    childrenStrokes = _.sum(svg.g.map(extractSvgPathCount))
+  }
+  return selfStrokes + childrenStrokes
+}
+
+const countStrokes = svg => extractSvgPathCount(getSvgRoot(svg))
 
 const weightComponents = comps => {
   return _.flatMap(comps, c => {
@@ -58,14 +74,16 @@ const weightComponents = comps => {
   })
 }
 
-const similarity = (weightedComponents1, weightedComponents2) => {
+// Usually returns values below 2, with maximum at about 4
+const similarity = (weightedComponents1, strokeCount1, weightedComponents2, strokeCount2) => {
   let common = _.intersection(
     _.uniq(weightedComponents1.map(c => c.component)),
     _.uniq(weightedComponents2.map(c => c.component))
   )
   let w1 = weightedComponents1.map(c => _.includes(common, c.component) ? c.weight : 0)
   let w2 = weightedComponents2.map(c => _.includes(common, c.component) ? c.weight : 0)
-  return _.sum(w1) + _.sum(w2)
+  let strokeCountRatio = Math.min(strokeCount1, strokeCount2) / Math.max(strokeCount1, strokeCount2)
+  return (_.sum(w1) + _.sum(w2)) * strokeCountRatio
 }
 
 const buildSvgData = kanjiList => Promise.all(
@@ -106,6 +124,12 @@ Promise.join(
           .value()
         // console.log(componentsMap)
 
+        let strokeCountMap = _(svgData)
+          .map((svg, kanji) => [kanji, countStrokes(svg)])
+          .fromPairs()
+          .value()
+        // console.log(strokeCountMap)
+
         console.log('Calculating component weigths...')
         let weightedComponentsMap = _(componentsMap)
           .map((comps, kanji) => [kanji, weightComponents(comps)])
@@ -121,12 +145,12 @@ Promise.join(
             .map(otherKanji => ({
               kanji: otherKanji,
               similarity: similarity(
-                weightedComponentsMap[thisKanji],
-                weightedComponentsMap[otherKanji]
+                weightedComponentsMap[thisKanji], strokeCountMap[thisKanji],
+                weightedComponentsMap[otherKanji], strokeCountMap[otherKanji]
               )
             }))
             .sort((a, b) => b.similarity - a.similarity)
-            .takeWhile((o, idx) => idx < 20 && o.similarity > 0.5)
+            .takeWhile((o, idx) => idx < 20 || o.similarity > 3)
             .map(o => o.kanji)
             .value()
             .join('')
